e0f5ed7355f379a2c50e063840930bdb088e05a0
Yung Ling Hou <houy062924@gmail.com>
2020-07-03 11:53:44 +0800

    Revert "Revert "redesigned card""
    This reverts commit 4a34cea25ef47969e1072f44398f569ef5e0afbb.

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,19 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import 'materialize-css/dist/css/materialize.min.css';
 
 class Nav extends React.Component {
   render() {
     return (
-      <nav>
-        <div className="nav-wrapper">
-          <div className="brand-logo"><Link to="/">LOGO</Link></div>
-          <ul className="right">
-            <li><Link to="/dashboard">ORG DASHBOARD</Link></li>
-            <li><Link to="/swipe">SWIPE</Link></li>
-          </ul>
-        </div>
-      </nav>
+      <div className="navCont">
+        <ul>
+          <li><Link to="/dashboard">ORG DASHBOARD</Link></li>
+          <li><Link to="/swipe">SWIPE</Link></li>
+        </ul>
+      </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./styles/card.css";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import Nav from "./Components/Nav";
 import Carousel from "./Components/Carousel";
-import OrgDashboard from "./pages/orgDashboard";
+import Orgs from "./pages/orgs";
 
 
 class App extends React.Component {
@@ -15,9 +15,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Nav></Nav>
 
-
-        <Route exact path="/" component={OrgDashboard}></Route>
-        <Route path="/dashboard" component={OrgDashboard}></Route>
+        <Route path="/dashboard" component={Orgs}></Route>
         <Route path="/swipe" component={Carousel}></Route>
 
       </BrowserRouter>
