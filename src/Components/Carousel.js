53606caa8452004bf682a77ed77269805a0e60d3
Yung Ling Hou <houy062924@gmail.com>
2020-07-06 09:14:52 +0800

    small fixes

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -8,18 +8,21 @@ class Carousel extends React.Component {
   constructor() {
     super();
     this.state = {
-      profiles: []
+      profiles: [],
+      likeList: []
     }
+
+    this.handleLike = this.handleLike.bind(this);
   }
 
   componentDidMount() {
-    const elem = document.querySelector(".carousel");
-    const options = {
-      numVisible: 3
-    };
-    const instance = M.Carousel.init(elem, options);
+    // const elem = document.querySelector(".carousel");
+    // const options = {
+    //   numVisible: 3
+    // };
+    // const instance = M.Carousel.init(elem, options);
 
-    // get data
+    // get data from database
     const profiles = []
     const db = firebase.firestore();
     const data = db.collection("animals").get()
@@ -32,6 +35,26 @@ class Carousel extends React.Component {
         profiles: profiles
       })
     });
+
+    // get data from local storage
+    if ( localStorage.getItem("likes") !== null ) {
+      this.setState({
+        likeList: JSON.parse(localStorage.getItem("likes"))
+      })
+    }
+  }
+
+  handleLike(eTarget, propProfile, propLikes) {
+    // if (localStorage.getItem("likes") === null ) {
+      let newlikes = [...this.state.likeList, propProfile]
+      this.setState({
+        likeList: newlikes
+      })
+      localStorage.setItem("likes", JSON.stringify(newlikes))
+      eTarget.disabled = true;
+    // }
+
+    
   }
   
   render() {
@@ -39,7 +62,7 @@ class Carousel extends React.Component {
       <div id="carouselCont">
         { this.state.profiles !== undefined &&
           this.state.profiles.map((profile) => (
-            <Card profile={profile} key={profile.id} />
+            <Card profile={profile} likes={this.state.likeList} key={profile.id} handleLike={this.handleLike} />
           ))
         }
 
@@ -58,11 +81,32 @@ class Card extends React.Component {
     this.handleLike = this.handleLike.bind(this);
   }
 
-  handleLike() {
-    console.log("click")
+  handleLike(e) {
+    this.props.handleLike(e.target, this.props.profile, this.props.likes);
+
+    // if ( localStorage.getItem("likes") === null ) {
+    //   likesArr.push(this.props.profile);
+    //   localStorage.setItem("likes", JSON.stringify(likesArr));
+    //   e.target.disabled = true;
+    //   e.target.textContent = "已加入";
+    // }
+    // else {
+    //   likesArr = JSON.parse(localStorage.getItem("likes"));
+    //   likesArr.push(this.props.profile);
+    //   localStorage.setItem("likes", JSON.stringify(likesArr));
+    //   e.target.disabled = true;
+    //   e.target.textContent = "已加入";
+    // }
   }
 
   render() {
+    let liked = false;
+    this.props.likes.forEach((like)=>{
+      if ( like.id === this.props.profile.id ) {
+        liked = true;
+      } 
+    })
+
     return (
       <div className="cardCont">
         <div className="imgCont">
@@ -85,7 +129,10 @@ class Card extends React.Component {
           <p className="profileLocation">LOCATION</p>
           <p className="profileStory">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-          <button onClick={this.handleLike}>Like</button>
+          { liked 
+            ? <button type="button" disabled>已加入</button>
+            : <button type="button" onClick={this.handleLike}>Like</button>
+          }
         </div>
 
 
